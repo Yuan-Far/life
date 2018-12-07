a4aea6ccc0004c8c652296479a2bdea22b5e6ff4
JunYing <junying@pinduoduo.com>
2018-12-07 11:45:55 +0800

    refactor(del): del

--- src/components/LifeIndex/index.tsx
+++ src/components/LifeIndex/index.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
-
+import { withErrorBoundary } from '../../common/HOCComponent/HocErrorrBoundary/index';
+import { BaseError } from '../../common/ErrorMsg/index';
 import './style.less';
 
-class LifeIndex extends React.Component<{}> {
-    render() {
-        return (
-            <div>lifeindex</div>
-        );
-    }
-}
+const ErrorMessageWithErrorBoundary = withErrorBoundary(BaseError);
+const LifeIndex = () => (
+    <button type="button" onClick={() => { throw new Error(`Catch me!`); }}>
+        {`Throw nasty error`}
+    </button >
+  );
 
-export default LifeIndex;
+export default () => (
+    <ErrorMessageWithErrorBoundary>
+        <LifeIndex />
+    </ErrorMessageWithErrorBoundary>
+);
